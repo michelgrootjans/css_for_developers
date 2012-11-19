c5b4309deb44592f4ab7ab5bb11c0ae2c8fa8d13
Michel Grootjans <michel.grootjans@gmail.com>
2012-11-19 20:17:15 +0100

    generated new rails application

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'therubyracer'
+  gem 'less-rails'
+  gem 'twitter-bootstrap-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssForDevelopers::Application.config.secret_token = 'ab49c580e45b0ad41c8fe276d99e215c15b1c6e46de7de3677e191d641226199f3e1ef7a9c9f0230c3a7a85c39a752f0dc9c15381f63a08efadfafa81ff0acc8'
