2ee1fe983db1c101ac3062f125126e92e3f860d6
Michel Grootjans <michel.grootjans@gmail.com>
2012-12-12 10:32:07 +0100

    added submenu

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,5 @@
 CssForDevelopers::Application.routes.draw do
+  root :to => 'pages#index'
 
   resources :pages
-
-
-  get "css/index"
-
-  get "css/css"
-
-  get "css/less"
-
-  get "css/sass"
-
-  get "css/twitter_bootstrap"
-
-  get "css/zurb"
-
-  get "css/blueprint"
-
-  get "javascript/jquery"
-
-  get "javascript/jquery_ui"
-
-  get "javascript/node"
-
-  get "javascript/backbone"
-
-  get "dotnet/mvc"
-
-  get "dotnet/silverlight"
-
-  get "dotnet/utilities"
-
-  root :to => 'home#index'
-
-  match ':controller(/:action)'
 end
